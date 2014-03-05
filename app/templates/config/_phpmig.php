df06a424f0331d7ebe7b25c19fa11486f07ecb79
Robert Yokota <ryokota@yammer-inc.com>
2014-03-04 20:22:13 -0800

    Minor fixes

--- app/templates/config/_phpmig.php
+++ app/templates/config/_phpmig.php
@@ -10,13 +10,13 @@ $container['config'] = array(
     'driver'    => 'sqlite',
     'database'  => '/tmp/my.db',
     'prefix'    => '',
-)
+);
 
 $container['schema'] = $container->share(function($container) {
     /* Bootstrap Eloquent */
     $capsule = new Capsule;
     $capsule->addConnection($container['config']);
-    //$capsule->setAsGlobal();
+    $capsule->setAsGlobal();
     /* Bootstrap end */
 
     return Capsule::schema();
@@ -25,10 +25,10 @@ $container['schema'] = $container->share(function($container) {
 $container['phpmig.adapter'] = $container->share(function() use ($container) {
     $capsule = new Capsule;
     $capsule->addConnection($container['config']);
+    $capsule->setAsGlobal();
     return new Adapter\Illuminate\Database($capsule, 'migrations');
 });
 
 $container['phpmig.migrations_path'] = __DIR__ . DIRECTORY_SEPARATOR . 'migrations';
 
 return $container;
-
